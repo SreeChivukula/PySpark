d30692469256acc7dbdd1b5fbd1caa3c9b1116a8
SreeChivukula <53124063+SreeChivukula@users.noreply.github.com>
2020-04-07 21:18:13 -0400

    Add files via upload

---
+++ Basic_RDD.py
@@ -0,0 +1,191 @@
+from pyspark import SparkContext, SparkConf
+
+conf = SparkConf().setMaster('local').setAppName('BasicRDD')
+sc = SparkContext(conf=conf)
+
+
+#-----------------------------------------------------------------------------------------------
+# RDD "filter" Transformation
+# Reads all records/lines of an RDD and returns that matches the condition
+# Similar to SQL WHERE clause
+#-----------------------------------------------------------------------------------------------
+# import json
+# def filter_with_zip(line):
+#     line = json.loads(line)
+#     zipcode = line['address']['zipcode']
+#     zipcode = int(zipcode)
+#     if str(line['borough']) == "Brooklyn" and str(line['cuisine']) == "American ":
+#         return line
+    # if (zipcode > 11000):
+    #     return line
+
+# rdd1 = sc.textFile("restaurants.json")
+# for i in rdd1.take(10):
+#     print(i)
+
+# rdd2 = rdd1.filter(lambda x: filter_with_zip(x))
+# for i in rdd2.take(5):
+#     print(i)
+
+# # Write the above in a single line lambda function
+
+# for i in rdd2.collect(): # "collect" is an RDD "action"
+#     print(i)
+#     var=json.loads(i)
+#     print(var['address']['zipcode'])
+#
+# print('Total records in rdd1: ',rdd1.count())
+# print('Total records in rdd2: ',rdd2.count())
+
+
+#-----------------------------------------------------------------------------------------------
+# RDD "map" Transformation
+# Map reads all records one by one, does processing on those records and returns the exact
+# no. of records in the output rdd.
+#-----------------------------------------------------------------------------------------------
+# rdd1 = sc.textFile("file:///Users/SreeC/PySpark/sample-data.csv")
+# #
+# # def get_3_fields(record):
+# #     x,y,z = record.split(',')[0],record.split(',')[3],record.split(',')[5]
+# #     return x,y,z
+# #
+# rdd2 = rdd1.map(lambda x: (x.split(',')[0], x.split(',')[3], x.split(',')[5]))
+# #
+# for i in rdd2.collect():
+#     print(list(i))
+
+
+#-----------------------------------------------------------------------------------------------
+# RDD "flatMap" transformation
+# Takes each record from an RDD and returns multiple rows
+#-----------------------------------------------------------------------------------------------
+rdd1 = sc.textFile("file:///Users/SreeC/PySpark/stream-data.csv")
+
+# def remove_date(record):
+#     record = record.split('>')
+#     return record[1]
+
+# rdd2 = rdd1.map(lambda x: x.split('>')[1])
+# for i in rdd2.collect():
+#     print(i)
+# rdd3 = rdd2.flatMap(lambda x: x.split('|'))
+# for i in rdd3.collect():
+#     print(i)
+# rdd4 = rdd3.map(lambda x: (x.split('-')[0], x.split('-')[1]))
+# for i in rdd4.collect():
+#     print(i)
+
+
+#-----------------------------------------------------------------------------------------------
+# RDD "union/intersection" transformation
+# UNION Merges two RDDs - does not matter whether the RDDs have same data or not. But a better use
+# case would be to merge two RDDs with same data structure or schema. UNION WILL NOT REMOVE
+# DUPLICATE VALUES.
+# Intersection fetches common values from 2 RDDs. It will also remove duplicates whilst doing
+# the intersection.
+#-----------------------------------------------------------------------------------------------
+
+# rdd1 = sc.textFile("/Users/SreeC/PySpark/sample-data.csv")
+# rdd2 = sc.textFile("/Users/SreeC/PySpark/sample-data2.csv")
+# rdd3 = sc.textFile("/Users/SreeC/PySpark/stream_data.csv")
+#
+# union_rdd = rdd1.union(rdd2)
+# print(union_rdd.count())
+#
+# intersection_rdd = rdd1.intersection(rdd2)
+#
+# # for i in union_rdd.collect():
+# # for i in union_rdd.take(50):
+# for i in intersection_rdd.collect():
+#     print(i)
+
+
+#-----------------------------------------------------------------------------------------------
+# RDD "sortBy" transformation
+# Use to sort the entire RDD based on ONE key. We can specify the key ourselves
+#-----------------------------------------------------------------------------------------------
+# rdd = sc.textFile("/Users/SreeC/PySpark/2015-12-12.csv")
+# #
+# def convertToInteger(record):
+#     record = record.split(',')
+#     size = record[2]
+#     try:
+#         size = int(size)
+#         return record
+#     except ValueError:
+#         pass
+# #
+# rdd1 = rdd.filter(lambda x: convertToInteger(x))
+# rdd2 = rdd1.sortBy(lambda x: x.split(',')[2],ascending=True)
+# #
+# for i in rdd2.take(20):
+#     print(i)
+
+#-----------------------------------------------------------------------------------------------
+# RDD "groupBy" transformation
+# Similar to sortBy. We will have to pass the key based on which we want to group the
+# elements of the RDD.
+#-----------------------------------------------------------------------------------------------
+# rdd = sc.textFile("/Users/SreeC/PySpark/violations-data.csv")
+#
+# rdd1 = rdd.map(lambda x: (x.split(',')[0], x.split(',')[3]))
+# # for i in rdd1.collect():
+# #     print(i)
+# rdd2 = rdd1.groupBy(lambda x: x[0])
+# # for i in rdd2.collect():
+# #     print(i)
+# rdd3 = rdd2.map(lambda x:(x[0],list(x[1])))
+# #
+# # for i in rdd3.take(5):
+# for i in rdd3.collect():
+#     print(i)
+
+#-----------------------------------------------------------------------------------------------
+# RDD "reduce" transformation
+#-----------------------------------------------------------------------------------------------z
+# carRDD = sc.textFile("/Users/SreeC/PySpark/car_sales_data.csv")
+#
+# from decimal import Decimal, InvalidOperation
+# def trim_price(record):
+#     price = record.split(',')[4]
+#     price = price.strip('$')
+#
+#     try:
+#         try:
+#             price = Decimal(price)
+#             return price
+#         except InvalidOperation:
+#             pass
+#
+#     except ValueError:
+#         pass
+
+# def filter_none(record):
+#     if record is not None:
+#         return record
+#
+# rdd1 = carRDD.map(lambda x: trim_price(x))
+# for i in rdd1.take(5):
+#     print(i)
+# rdd2 = rdd1.filter(lambda x: filter_none(x))
+# for i in rdd2.take(5):
+#     print(i)
+# rdd_total = rdd2.reduce(lambda x,y: x+y)
+# rdd_max = rdd2.reduce(lambda x,y: max(x,y))
+# rdd_min = rdd2.reduce(lambda x,y: min(x,y))
+
+# print("Total Sales: ",rdd_total)
+# print("Max Sales: ",rdd_max)
+# print("Min Sales: ",rdd_min)
+
+
+# def total_records_per_partition(record):
+#     total_record = 0
+#     for x in record:
+#         total_record = total_record + 1
+#     return total_record
+#
+# rdd2 = rdd1.mapPartitions(lambda x: total_records_per_partition(x))
+#
+# for i in rdd2.collect():
+#     print(i)
